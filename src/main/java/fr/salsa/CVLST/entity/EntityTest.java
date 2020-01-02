f061adc8495941c29372677f552f6a5f8c4db1fd
Salsabanana <59451933+Salsabanana@users.noreply.github.com>
2020-01-02 20:36:58 +0100

    Add files via upload

--- src/main/java/fr/salsa/CVLST/entity/EntityTest.java
+++ src/main/java/fr/salsa/CVLST/entity/EntityTest.java
@@ -1,31 +1,157 @@
-package fr.salsa.CVLST.entity;
+package fr.salsa.CVLST.entity.models;
 
-import net.minecraft.entity.EntityAgeable;
-import net.minecraft.entity.passive.EntityCow;
-import net.minecraft.util.DamageSource;
-import net.minecraft.util.SoundEvent;
-import net.minecraft.world.World;
+import net.minecraft.client.model.ModelBase;
+import net.minecraft.client.model.ModelRenderer;
+import net.minecraft.entity.Entity;
 
-public class EntityTest extends EntityCow {
+/**
+ * ModelGuardian - Either Mojang or a mod author
+ * Created using Tabula 7.1.0
+ */
+public class EntityTest extends ModelBase {
+    public ModelRenderer field_178710_a0;
+    public ModelRenderer field_178710_a1;
+    public ModelRenderer field_178710_a2;
+    public ModelRenderer field_178710_a3;
+    public ModelRenderer field_178710_a4;
+    public ModelRenderer field_178710_aChild;
+    public ModelRenderer field_178710_aChild_1;
+    public ModelRenderer field_178710_aChild_2;
+    public ModelRenderer field_178710_aChild_3;
+    public ModelRenderer field_178710_aChild_4;
+    public ModelRenderer field_178710_aChild_5;
+    public ModelRenderer field_178710_aChild_6;
+    public ModelRenderer field_178710_aChild_7;
+    public ModelRenderer field_178710_aChild_8;
+    public ModelRenderer field_178710_aChild_9;
+    public ModelRenderer field_178710_aChild_10;
+    public ModelRenderer field_178710_aChild_11;
+    public ModelRenderer field_178710_aChild_12;
+    public ModelRenderer field_178710_aChild_13;
+    public ModelRenderer field_178710_aChildChild;
+    public ModelRenderer field_178710_aChildChildChild0;
+    public ModelRenderer field_178710_aChildChildChild1;
 
-    public EntityTest(World worldIn) {
-        super(worldIn);
-    }
-    @Override
-    public EntityCow createChild(EntityAgeable ageable){
-        return new EntityCow(world);
+    public EntityTest() {
+        this.textureWidth = 64;
+        this.textureHeight = 64;
+        this.field_178710_a4 = new ModelRenderer(this, 16, 40);
+        this.field_178710_a4.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.field_178710_a4.addBox(-6.0F, 22.0F, -6.0F, 12, 2, 12, 0.0F);
+        this.field_178710_aChild_10 = new ModelRenderer(this, 0, 0);
+        this.field_178710_aChild_10.setRotationPoint(3.5328707695007324F, 19.53287124633789F, 0.0F);
+        this.field_178710_aChild_10.addBox(-1.0F, -4.5F, -1.0F, 2, 9, 2, 0.0F);
+        this.setRotateAngle(field_178710_aChild_10, 0.0F, 0.0F, 2.356194496154785F);
+        this.field_178710_aChild_4 = new ModelRenderer(this, 0, 0);
+        this.field_178710_aChild_4.setRotationPoint(-3.54770565032959F, 16.0F, -3.54770565032959F);
+        this.field_178710_aChild_4.addBox(-1.0F, -4.5F, -1.0F, 2, 9, 2, 0.0F);
+        this.setRotateAngle(field_178710_aChild_4, 1.5707963705062866F, 0.7853981852531433F, 0.0F);
+        this.field_178710_aChild_6 = new ModelRenderer(this, 0, 0);
+        this.field_178710_aChild_6.setRotationPoint(3.6768126487731934F, 16.0F, 3.6768126487731934F);
+        this.field_178710_aChild_6.addBox(-1.0F, -4.5F, -1.0F, 2, 9, 2, 0.0F);
+        this.setRotateAngle(field_178710_aChild_6, 1.5707963705062866F, 3.9269909858703613F, 0.0F);
+        this.field_178710_aChild_11 = new ModelRenderer(this, 0, 0);
+        this.field_178710_aChild_11.setRotationPoint(-3.6003527641296387F, 19.600353240966797F, 0.0F);
+        this.field_178710_aChild_11.addBox(-1.0F, -4.5F, -1.0F, 2, 9, 2, 0.0F);
+        this.setRotateAngle(field_178710_aChild_11, 0.0F, 0.0F, 3.9269909858703613F);
+        this.field_178710_a2 = new ModelRenderer(this, 0, 28);
+        this.field_178710_a2.mirror = true;
+        this.field_178710_a2.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.field_178710_a2.addBox(6.0F, 10.0F, -6.0F, 2, 12, 12, 0.0F);
+        this.field_178710_aChild_1 = new ModelRenderer(this, 0, 0);
+        this.field_178710_aChild_1.setRotationPoint(0.0F, 12.356773376464844F, -3.643226146697998F);
+        this.field_178710_aChild_1.addBox(-1.0F, -4.5F, -1.0F, 2, 9, 2, 0.0F);
+        this.setRotateAngle(field_178710_aChild_1, 0.7853981852531433F, 0.0F, 0.0F);
+        this.field_178710_aChild_7 = new ModelRenderer(this, 0, 0);
+        this.field_178710_aChild_7.setRotationPoint(-3.6603150367736816F, 16.0F, 3.6603150367736816F);
+        this.field_178710_aChild_7.addBox(-1.0F, -4.5F, -1.0F, 2, 9, 2, 0.0F);
+        this.setRotateAngle(field_178710_aChild_7, 1.5707963705062866F, 2.356194496154785F, 0.0F);
+        this.field_178710_aChildChildChild1 = new ModelRenderer(this, 25, 19);
+        this.field_178710_aChildChildChild1.setRotationPoint(0.5F, 0.5F, 6.0F);
+        this.field_178710_aChildChildChild1.addBox(1.0F, 10.5F, 3.0F, 1, 9, 9, 0.0F);
+        this.setRotateAngle(field_178710_aChildChildChild1, 0.0F, 0.10161735862493515F, 0.0F);
+        this.field_178710_aChild_2 = new ModelRenderer(this, 0, 0);
+        this.field_178710_aChild_2.setRotationPoint(3.566713333129883F, 12.433286666870117F, 0.0F);
+        this.field_178710_aChild_2.addBox(-1.0F, -4.5F, -1.0F, 2, 9, 2, 0.0F);
+        this.setRotateAngle(field_178710_aChild_2, 0.0F, 0.0F, 0.7853981852531433F);
+        this.field_178710_aChild = new ModelRenderer(this, 0, 0);
+        this.field_178710_aChild.setRotationPoint(0.0F, 12.319999694824219F, 3.679999828338623F);
+        this.field_178710_aChild.addBox(-1.0F, -4.5F, -1.0F, 2, 9, 2, 0.0F);
+        this.setRotateAngle(field_178710_aChild, 5.4977874755859375F, 0.0F, 0.0F);
+        this.field_178710_a0 = new ModelRenderer(this, 0, 0);
+        this.field_178710_a0.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.field_178710_a0.addBox(-6.0F, 10.0F, -8.0F, 12, 12, 16, 0.0F);
+        this.field_178710_aChild_8 = new ModelRenderer(this, 0, 0);
+        this.field_178710_aChild_8.setRotationPoint(0.0F, 19.588359832763672F, 3.58836030960083F);
+        this.field_178710_aChild_8.addBox(-1.0F, -4.5F, -1.0F, 2, 9, 2, 0.0F);
+        this.setRotateAngle(field_178710_aChild_8, 3.9269909858703613F, 0.0F, 0.0F);
+        this.field_178710_aChild_13 = new ModelRenderer(this, 40, 0);
+        this.field_178710_aChild_13.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.field_178710_aChild_13.addBox(-2.0F, 14.0F, 7.0F, 4, 4, 8, 0.0F);
+        this.setRotateAngle(field_178710_aChild_13, 0.0F, 0.03387245163321495F, 0.0F);
+        this.field_178710_aChildChildChild0 = new ModelRenderer(this, 25, 19);
+        this.field_178710_aChildChildChild0.setRotationPoint(0.5F, 0.5F, 6.0F);
+        this.field_178710_aChildChildChild0.addBox(0.0F, 14.0F, 0.0F, 2, 2, 6, 0.0F);
+        this.setRotateAngle(field_178710_aChildChildChild0, 0.0F, 0.10161735862493515F, 0.0F);
+        this.field_178710_aChild_3 = new ModelRenderer(this, 0, 0);
+        this.field_178710_aChild_3.setRotationPoint(-3.5208005905151367F, 12.479199409484863F, 0.0F);
+        this.field_178710_aChild_3.addBox(-1.0F, -4.5F, -1.0F, 2, 9, 2, 0.0F);
+        this.setRotateAngle(field_178710_aChild_3, 0.0F, 0.0F, 5.4977874755859375F);
+        this.field_178710_aChild_9 = new ModelRenderer(this, 0, 0);
+        this.field_178710_aChild_9.setRotationPoint(0.0F, 19.527111053466797F, -3.5271105766296387F);
+        this.field_178710_aChild_9.addBox(-1.0F, -4.5F, -1.0F, 2, 9, 2, 0.0F);
+        this.setRotateAngle(field_178710_aChild_9, 2.356194496154785F, 0.0F, 0.0F);
+        this.field_178710_a1 = new ModelRenderer(this, 0, 28);
+        this.field_178710_a1.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.field_178710_a1.addBox(-8.0F, 10.0F, -6.0F, 2, 12, 12, 0.0F);
+        this.field_178710_aChild_12 = new ModelRenderer(this, 8, 0);
+        this.field_178710_aChild_12.setRotationPoint(0.0F, 0.0F, -8.25F);
+        this.field_178710_aChild_12.addBox(-1.0F, 15.0F, 0.0F, 2, 2, 1, 0.0F);
+        this.field_178710_a3 = new ModelRenderer(this, 16, 40);
+        this.field_178710_a3.setRotationPoint(0.0F, 0.0F, 0.0F);
+        this.field_178710_a3.addBox(-6.0F, 8.0F, -6.0F, 12, 2, 12, 0.0F);
+        this.field_178710_aChild_5 = new ModelRenderer(this, 0, 0);
+        this.field_178710_aChild_5.setRotationPoint(3.6226859092712402F, 16.0F, -3.6226859092712402F);
+        this.field_178710_aChild_5.addBox(-1.0F, -4.5F, -1.0F, 2, 9, 2, 0.0F);
+        this.setRotateAngle(field_178710_aChild_5, 1.5707963705062866F, 5.4977874755859375F, 0.0F);
+        this.field_178710_aChildChild = new ModelRenderer(this, 0, 54);
+        this.field_178710_aChildChild.setRotationPoint(-1.5F, 0.5F, 14.0F);
+        this.field_178710_aChildChild.addBox(0.0F, 14.0F, 0.0F, 3, 3, 7, 0.0F);
+        this.setRotateAngle(field_178710_aChildChild, 0.0F, 0.0677449032664299F, 0.0F);
+        this.field_178710_a0.addChild(this.field_178710_aChild_10);
+        this.field_178710_a0.addChild(this.field_178710_aChild_4);
+        this.field_178710_a0.addChild(this.field_178710_aChild_6);
+        this.field_178710_a0.addChild(this.field_178710_aChild_11);
+        this.field_178710_a0.addChild(this.field_178710_aChild_1);
+        this.field_178710_a0.addChild(this.field_178710_aChild_7);
+        this.field_178710_aChildChild.addChild(this.field_178710_aChildChildChild1);
+        this.field_178710_a0.addChild(this.field_178710_aChild_2);
+        this.field_178710_a0.addChild(this.field_178710_aChild);
+        this.field_178710_a0.addChild(this.field_178710_aChild_8);
+        this.field_178710_a0.addChild(this.field_178710_aChild_13);
+        this.field_178710_aChildChild.addChild(this.field_178710_aChildChildChild0);
+        this.field_178710_a0.addChild(this.field_178710_aChild_3);
+        this.field_178710_a0.addChild(this.field_178710_aChild_9);
+        this.field_178710_a0.addChild(this.field_178710_aChild_12);
+        this.field_178710_a0.addChild(this.field_178710_aChild_5);
+        this.field_178710_aChild_13.addChild(this.field_178710_aChildChild);
     }
 
     @Override
-    protected SoundEvent getAmbientSound(){
-        return super.getAmbientSound();
+    public void render(Entity entity, float f, float f1, float f2, float f3, float f4, float f5) { 
+        this.field_178710_a4.render(f5);
+        this.field_178710_a2.render(f5);
+        this.field_178710_a0.render(f5);
+        this.field_178710_a1.render(f5);
+        this.field_178710_a3.render(f5);
     }
-    @Override
-    protected SoundEvent getHurtSound(DamageSource source) {
-        return super.getHurtSound(source);
-    }
-    @Override
-    protected SoundEvent getDeathSound(){
-        return super.getDeathSound();
+
+    /**
+     * This is a helper function from Tabula to set the rotation of model parts
+     */
+    public void setRotateAngle(ModelRenderer modelRenderer, float x, float y, float z) {
+        modelRenderer.rotateAngleX = x;
+        modelRenderer.rotateAngleY = y;
+        modelRenderer.rotateAngleZ = z;
     }
 }
