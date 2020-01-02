5e55b211ac470e513b9e0d4797dbf1feb6c2eaef
Salsabanana <59451933+Salsabanana@users.noreply.github.com>
2020-01-02 20:45:33 +0100

    Add files via upload

---
+++ src/main/java/fr/salsa/CVLST/util/References.java
@@ -0,0 +1,13 @@
+package fr.salsa.CVLST.utils;
+
+public class References {
+    public static final String MODID = "cvlst";
+    public static final String NAME = "Civilisations";
+    public static final String VERSION = "1.0";
+    public static final String ClientProxy = "fr.salsa.CVLST.proxy.ClientProxy";
+    public static final String ServerProxy = "fr.salsa.CVLST.proxy.ServerProxy";
+
+
+
+    public static final int Entity_Test = 120;
+}
