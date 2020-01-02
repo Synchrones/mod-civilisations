0767e7d5ac9c8c7dd477e826dd33cdae973ec900
Salsabanana <59451933+Salsabanana@users.noreply.github.com>
2020-01-02 20:32:31 +0100

    Add files via upload

---
+++ src/main/java/fr/salsa/CVLST/CVLSTBlocks.java
@@ -0,0 +1,22 @@
+package fr.salsa.CVLST.blocks;
+
+import fr.salsa.CVLST.ModMain;
+import fr.salsa.CVLST.init.ModBlocks;
+import fr.salsa.CVLST.init.ModItems;
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.item.ItemBlock;
+
+public class CVLSTBlocks extends Block {
+
+    public CVLSTBlocks(String name, Material materialIn, float hardness, float resistance, int harvestLevel, String harvestType) {
+        super(materialIn);
+        setRegistryName(name).setUnlocalizedName(name);
+        setCreativeTab(ModMain.modtab);
+        setHardness(hardness);
+        setResistance(resistance);
+        setHarvestLevel(harvestType, harvestLevel);
+        ModBlocks.BLOCKS.add(this);
+        ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+    }
+}
