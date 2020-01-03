22be99ba3d5da8f4639f2691facf509326be14b3
Salsabanana <59451933+Salsabanana@users.noreply.github.com>
2020-01-03 16:23:47 +0100

    Update CVLSTSaplings.java

--- src/main/java/fr/salsa/CVLST/blocks/tree/CVLSTSaplings.java
+++ src/main/java/fr/salsa/CVLST/blocks/tree/CVLSTSaplings.java
@@ -4,13 +4,59 @@ import fr.salsa.CVLST.ModMain;
 import fr.salsa.CVLST.init.ModBlocks;
 import fr.salsa.CVLST.init.ModItems;
 import net.minecraft.block.BlockSapling;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyInteger;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.item.ItemBlock;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+
+import javax.annotation.Nullable;
 
 public class CVLSTSaplings extends BlockSapling {
+    public static final PropertyInteger STAGE = PropertyInteger.create("stage", 0, 1);
+    protected static final AxisAlignedBB SAPLING_AABB = new AxisAlignedBB(0.09999999403953552D, 0.0D, 0.09999999403953552D, 0.8999999761581421D, 0.800000011920929D, 0.8999999761581421D);
     public CVLSTSaplings(String name) {
         setRegistryName(name).setUnlocalizedName(name);
         setCreativeTab(ModMain.modtab);
+        setDefaultState(this.blockState.getBaseState().withProperty(STAGE, Integer.valueOf(0)));
         ModBlocks.BLOCKS.add(this);
         ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
     }
+    @Override
+    public IBlockState getStateFromMeta(int meta) {
+        return this.getDefaultState().withProperty(STAGE, Integer.valueOf((meta & 8)>> 3));
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state) {
+        int i = 0;
+        i = i | ((Integer)state.getValue(STAGE)).intValue() <<3;
+        return i;
+    }
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, new IProperty[]{ STAGE});
+    }
+    @Override
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
+        return SAPLING_AABB;
+    }
+    @Nullable
+    @Override
+    public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
+        return NULL_AABB;
+    }
+
+    @Override
+    public boolean isOpaqueCube(IBlockState state) {
+        return false;
+    }
+    @Override
+    public boolean isFullCube(IBlockState state) {
+        return false;
+    }
+
 }
