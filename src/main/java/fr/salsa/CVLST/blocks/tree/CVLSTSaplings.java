3054bea5b5f317402986c00fad84ba68a158770b
Salsabanana <59451933+Salsabanana@users.noreply.github.com>
2020-01-03 17:16:51 +0100

    update de la class, reglage d'un bug
     réglage d'un bugqui mettait l'item 6 fois dans la ct

--- src/main/java/fr/salsa/CVLST/blocks/tree/CVLSTSaplings.java
+++ src/main/java/fr/salsa/CVLST/blocks/tree/CVLSTSaplings.java
@@ -3,7 +3,7 @@ package fr.salsa.CVLST.blocks.trees;
 import fr.salsa.CVLST.ModMain;
 import fr.salsa.CVLST.init.ModBlocks;
 import fr.salsa.CVLST.init.ModItems;
-import net.minecraft.block.BlockSapling;
+import net.minecraft.block.BlockBush;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockStateContainer;
@@ -12,22 +12,26 @@ import net.minecraft.item.ItemBlock;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
-
 import javax.annotation.Nullable;
 
-public class CVLSTSaplings extends BlockSapling {
+public class CVLSTSaplings extends BlockBush {
     public static final PropertyInteger STAGE = PropertyInteger.create("stage", 0, 1);
     protected static final AxisAlignedBB SAPLING_AABB = new AxisAlignedBB(0.09999999403953552D, 0.0D, 0.09999999403953552D, 0.8999999761581421D, 0.800000011920929D, 0.8999999761581421D);
     public CVLSTSaplings(String name) {
+        this.setDefaultState(this.blockState.getBaseState().withProperty(STAGE, Integer.valueOf(0)));
         setRegistryName(name).setUnlocalizedName(name);
         setCreativeTab(ModMain.modtab);
-        setDefaultState(this.blockState.getBaseState().withProperty(STAGE, Integer.valueOf(0)));
         ModBlocks.BLOCKS.add(this);
         ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
     }
     @Override
+    protected BlockStateContainer createBlockState() {
+        return new BlockStateContainer(this, new IProperty[] {STAGE});
+    }
+
+    @Override
     public IBlockState getStateFromMeta(int meta) {
-        return this.getDefaultState().withProperty(STAGE, Integer.valueOf((meta & 8)>> 3));
+        return this.getDefaultState().withProperty(STAGE , Integer.valueOf((meta & 8)>> 3));
     }
 
     @Override
@@ -36,10 +40,7 @@ public class CVLSTSaplings extends BlockSapling {
         i = i | ((Integer)state.getValue(STAGE)).intValue() <<3;
         return i;
     }
-    @Override
-    protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, new IProperty[]{ STAGE});
-    }
+
     @Override
     public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
         return SAPLING_AABB;
@@ -58,5 +59,4 @@ public class CVLSTSaplings extends BlockSapling {
     public boolean isFullCube(IBlockState state) {
         return false;
     }
-
 }
