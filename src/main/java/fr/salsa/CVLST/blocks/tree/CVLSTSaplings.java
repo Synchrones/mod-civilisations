bb6581df3c2baa89b101ea9794ebd31f62b6ff62
Salsabanana <59451933+Salsabanana@users.noreply.github.com>
2020-01-04 19:17:37 +0100

    Update CVLSTSaplings.java

--- src/main/java/fr/salsa/CVLST/blocks/tree/CVLSTSaplings.java
+++ src/main/java/fr/salsa/CVLST/blocks/tree/CVLSTSaplings.java
@@ -19,7 +19,7 @@ import net.minecraft.world.World;
 import javax.annotation.Nullable;
 import java.util.Random;
 
-public class CVLSTSaplings extends BlockBush {
+public class CVLSTSaplings extends BlockBush implements IGrowAble{
     public static final PropertyInteger STAGE = PropertyInteger.create("stage", 0, 1);
     protected static final AxisAlignedBB SAPLING_AABB = new AxisAlignedBB(0.09999999403953552D, 0.0D, 0.09999999403953552D, 0.8999999761581421D, 0.800000011920929D, 0.8999999761581421D);
 
@@ -86,6 +86,10 @@ public class CVLSTSaplings extends BlockBush {
         return state.getBlock() == Blocks.GRASS || state.getBlock() == Blocks.DIRT || state.getBlock() == Blocks.FARMLAND;
     }
 }
+ @Override
+    public void grow(World worldIn, Random rand, BlockPos pos, IBlockState state) {
+        
+    }
 //a mettre quand code arbre fini
     /*@Override
     public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
