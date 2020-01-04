62833336ed63495bfba58c5c97d86bc5c45e2276
Salsabanana <59451933+Salsabanana@users.noreply.github.com>
2020-01-04 19:22:14 +0100

    reglage bug
    reglage du bug qui mettait l'item 6 fois dans la modtab

--- src/main/java/fr/salsa/CVLST/blocks/tree/CVLSTPlank.java
+++ src/main/java/fr/salsa/CVLST/blocks/tree/CVLSTPlank.java
@@ -1,18 +1,19 @@
 package fr.salsa.CVLST.blocks.trees;
+package fr.salsa.CVLST.blocks.trees;
 
 import fr.salsa.CVLST.ModMain;
 import fr.salsa.CVLST.init.ModBlocks;
 import fr.salsa.CVLST.init.ModItems;
-import net.minecraft.block.BlockPlanks;
+import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.item.ItemBlock;
 
-public class CVLSTPlank extends BlockPlanks {
+public class CVLSTPlank extends Block {
     public CVLSTPlank(String name) {
+        super(Material.WOOD);
         setRegistryName(name).setUnlocalizedName(name);
         setCreativeTab(ModMain.modtab);
         ModBlocks.BLOCKS.add(this);
         ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
     }
 }
-
