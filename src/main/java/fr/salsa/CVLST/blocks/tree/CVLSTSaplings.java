f4e89b683bc3125744529c7991ab419b00bbd082
Salsabanana <59451933+Salsabanana@users.noreply.github.com>
2020-01-04 19:15:49 +0100

    Update CVLSTSaplings.java
    ajout de methode : bone meal et truc du genre

--- src/main/java/fr/salsa/CVLST/blocks/tree/CVLSTSaplings.java
+++ src/main/java/fr/salsa/CVLST/blocks/tree/CVLSTSaplings.java
@@ -4,40 +4,48 @@ import fr.salsa.CVLST.ModMain;
 import fr.salsa.CVLST.init.ModBlocks;
 import fr.salsa.CVLST.init.ModItems;
 import net.minecraft.block.BlockBush;
+import net.minecraft.block.SoundType;
 import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyInteger;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemBlock;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
 import javax.annotation.Nullable;
+import java.util.Random;
 
 public class CVLSTSaplings extends BlockBush {
     public static final PropertyInteger STAGE = PropertyInteger.create("stage", 0, 1);
     protected static final AxisAlignedBB SAPLING_AABB = new AxisAlignedBB(0.09999999403953552D, 0.0D, 0.09999999403953552D, 0.8999999761581421D, 0.800000011920929D, 0.8999999761581421D);
+
     public CVLSTSaplings(String name) {
         this.setDefaultState(this.blockState.getBaseState().withProperty(STAGE, Integer.valueOf(0)));
         setRegistryName(name).setUnlocalizedName(name);
         setCreativeTab(ModMain.modtab);
+        setSoundType(SoundType.PLANT);
         ModBlocks.BLOCKS.add(this);
         ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
     }
+
     @Override
     protected BlockStateContainer createBlockState() {
-        return new BlockStateContainer(this, new IProperty[] {STAGE});
+        return new BlockStateContainer(this, new IProperty[]{STAGE});
     }
 
     @Override
     public IBlockState getStateFromMeta(int meta) {
-        return this.getDefaultState().withProperty(STAGE , Integer.valueOf((meta & 8)>> 3));
+        return this.getDefaultState().withProperty(STAGE, Integer.valueOf((meta & 8) >> 3));
     }
 
     @Override
     public int getMetaFromState(IBlockState state) {
         int i = 0;
-        i = i | ((Integer)state.getValue(STAGE)).intValue() <<3;
+        i = i | ((Integer) state.getValue(STAGE)).intValue() << 3;
         return i;
     }
 
@@ -45,6 +53,7 @@ public class CVLSTSaplings extends BlockBush {
     public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess source, BlockPos pos) {
         return SAPLING_AABB;
     }
+
     @Nullable
     @Override
     public AxisAlignedBB getCollisionBoundingBox(IBlockState blockState, IBlockAccess worldIn, BlockPos pos) {
@@ -55,8 +64,39 @@ public class CVLSTSaplings extends BlockBush {
     public boolean isOpaqueCube(IBlockState state) {
         return false;
     }
+
     @Override
     public boolean isFullCube(IBlockState state) {
         return false;
     }
+
+    //code arbre
+    @Override
+    public boolean canGrow(World worldIn, BlockPos pos, IBlockState state, boolean isClient) {
+        return true;
+    }
+
+    @Override
+    public boolean canUseBonemeal(World worldIn, Random rand, BlockPos pos, IBlockState state) {
+        return (double) worldIn.rand.nextFloat() < 0.45D;
+    }
+
+    @Override
+    protected boolean canSustainBush(IBlockState state) {
+        return state.getBlock() == Blocks.GRASS || state.getBlock() == Blocks.DIRT || state.getBlock() == Blocks.FARMLAND;
+    }
 }
+//a mettre quand code arbre fini
+    /*@Override
+    public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
+        if (!worldIn.isRemote)
+        {
+            super.updateTick(worldIn, pos, state, rand);
+
+            if (worldIn.getLightFromNeighbors(pos.up()) >= 9 && rand.nextInt(7) == 0)
+            {
+                this.grow(worldIn, rand, pos, state);
+            }
+        }
+    }
+}*/
