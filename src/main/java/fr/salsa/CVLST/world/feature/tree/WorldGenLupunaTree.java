20b914218069a887329899cf55b0687d0919aa62
Salsabanana <59451933+Salsabanana@users.noreply.github.com>
2020-02-05 21:52:31 +0100

    Update WorldGenLupunaTree.java

--- src/main/java/fr/salsa/CVLST/world/feature/tree/WorldGenLupunaTree.java
+++ src/main/java/fr/salsa/CVLST/world/feature/tree/WorldGenLupunaTree.java
@@ -236,7 +236,7 @@ public class WorldGenLupunaTree extends WorldGenAbstractTree {
                     }
                     this.setBlockAndNotifyAdequately(world, branchpos.add(-3,2,0), leaf);
                     this.setBlockAndNotifyAdequately(world, branchpos.add(-3,2,2), leaf);
-                    break;
+                   
             }
         }
     }
