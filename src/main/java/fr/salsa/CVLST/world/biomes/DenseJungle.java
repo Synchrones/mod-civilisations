0e2645ea7946ad50a839e81adeb29088ab165fb2
Salsabanana <59451933+Salsabanana@users.noreply.github.com>
2020-02-23 21:22:02 +0100

    Update DenseJungle.java

--- src/main/java/fr/salsa/CVLST/world/biomes/DenseJungle.java
+++ src/main/java/fr/salsa/CVLST/world/biomes/DenseJungle.java
@@ -6,7 +6,7 @@ import net.minecraftforge.common.BiomeManager;
 
 public class DenseJungle extends Biome {
     public DenseJungle(){
-        super(new BiomeProperties("Dense Jungle").setTemperature(0.95F).setHeightVariation(0.2F).setRainfall(0.7F));
+        super(new BiomeProperties("Dense Jungle").setTemperature(0.95F).setHeightVariation(0.2F).setRainfall(0.7F).setBaseHeight(0.125F));
         this.decorator.treesPerChunk = 10;
         this.spawnableCreatureList.clear();
         this.spawnableCaveCreatureList.clear();
