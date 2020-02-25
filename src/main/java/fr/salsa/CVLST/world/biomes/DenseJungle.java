2d6b0d5d823153039ab290b021fdd27d472a0acb
Salsabanana <59451933+Salsabanana@users.noreply.github.com>
2020-02-25 22:33:45 +0100

    Update DenseJungle.java

--- src/main/java/fr/salsa/CVLST/world/biomes/DenseJungle.java
+++ src/main/java/fr/salsa/CVLST/world/biomes/DenseJungle.java
@@ -1,15 +1,22 @@
-package fr.salsa.CVLST.world.biomes;
-
-import fr.salsa.CVLST.init.ModBiomes;
+import fr.salsa.CVLST.world.feature.tree.WorldGenLupunaTree;
 import net.minecraft.world.biome.Biome;
-import net.minecraftforge.common.BiomeManager;
+import net.minecraft.world.gen.feature.WorldGenAbstractTree;
+
+import java.util.Random;
 
 public class DenseJungle extends Biome {
+    protected final WorldGenAbstractTree tree = new WorldGenLupunaTree();
     public DenseJungle(){
         super(new BiomeProperties("Dense Jungle").setTemperature(0.95F).setHeightVariation(0.2F).setRainfall(0.7F).setBaseHeight(0.125F));
+        this.decorator.treesPerChunk = 25;
         this.spawnableCreatureList.clear();
         this.spawnableCaveCreatureList.clear();
         this.spawnableMonsterList.clear();
         this.spawnableWaterCreatureList.clear();
+    }
+
+    @Override
+    public WorldGenAbstractTree getRandomTreeFeature(Random rand) {
+        return tree;
     }
 }
