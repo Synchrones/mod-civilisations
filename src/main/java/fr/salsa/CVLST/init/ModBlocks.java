df61ae80c766004352e50470fcd8ccb0fce31181
Salsabanana <59451933+Salsabanana@users.noreply.github.com>
2020-02-28 17:55:23 +0100

    Update ModBlocks.java

--- src/main/java/fr/salsa/CVLST/init/ModBlocks.java
+++ src/main/java/fr/salsa/CVLST/init/ModBlocks.java
@@ -1,15 +1,18 @@
 package fr.salsa.CVLST.init;
 
-import fr.salsa.CVLST.blocks.CVLSTBlocks;
-//import fr.salsa.CVLST.blocks.trees.CustomBlockSapling;//
+import fr.salsa.CVLST.blocks.*;
+import fr.salsa.CVLST.blocks.slabs.CVLSTSlabDouble;
+import fr.salsa.CVLST.blocks.slabs.CVLSTSlabHalf;
+import fr.salsa.CVLST.blocks.slabs.CVLSTSlabs;
 import fr.salsa.CVLST.blocks.trees.CVLSTLeaves;
 import fr.salsa.CVLST.blocks.trees.CVLSTLog;
 import fr.salsa.CVLST.blocks.trees.CVLSTSaplings;
-import fr.salsa.CVLST.blocks.trees.CVLSTPlank;
 import fr.salsa.CVLST.utils.References;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockSlab;
 import net.minecraft.block.material.Material;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.event.ModelRegistryEvent;
@@ -17,37 +20,25 @@ import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class ModBlocks {
-    public static final ModBlocks INSTANCE = new ModBlocks();
+    public static final ModItems INSTANCE = new ModItems();
     public static final List<Block> BLOCKS = new ArrayList<Block>();
     public static final Block rubyBlock = new CVLSTBlocks("ruby_block", Material.IRON, 5.0f, 30.0f, 1, "pickaxe");
     public static final Block saphirBlock = new CVLSTBlocks("saphir_block", Material.IRON, 5.0f, 30.0f, 1, "pickaxe");
-    public static final Block LupunaLog = new CVLSTLog("lupuna_log");
+    public static final Block lupunaLog = new CVLSTLog("lupuna_log");
     public static final Block lupunaPlank = new CVLSTBlocks("lupuna_plank", Material.WOOD,2.0F, 5.0F, 0, "axe");
-    public static final Block LupunaSapling = new CVLSTSaplings("lupuna_saplings");
+    public static final Block lupunaLeave = new CVLSTLeaves("lupuna_leave");
+    public static final Block lupunaSapling = new CVLSTSaplings("lupuna_sapling");
+    public static final Block lupuna_stair = new CVLSTStairs(Blocks.PLANKS.getDefaultState(), "lupuna_stair");
+    public static final BlockSlab lupuna_slab_double = (BlockSlab) new CVLSTSlabDouble("copper_slab_double", Material.IRON, ModBlocks.lupuna_slab_half).getDefaultState().withProperty(CVLSTSlabs.variant, CVLSTSlabs.Variant.lupuna);
+    public static final BlockSlab lupuna_slab_half = (BlockSlab) new CVLSTSlabHalf("copper_slab_half", Material.IRON, ModBlocks.lupuna_slab_half, ModBlocks.lupuna_slab_double).getDefaultState().withProperty(CVLSTSlabs.variant, CVLSTSlabs.Variant.lupuna);
 
-//a utiliser un jour ou l'autre...
-    /*public static final Block plank = new CustomBlockPlank("planks");
-    public static final Block leave = new CustomBlockLeave("leave");
-    public static final Block log = new CustomBlockLog("log");
-    public static final Block sapling = new CustomBlockSapling("sapling");
 
-    @SubscribeEvent
-    public static void registerRenders() {
-        for (int i = 0; i < EnumHandler.Enumtype.values().length; i++) {
-            registerRender(plank, i, "plank_" + EnumHandler.Enumtype.values()[i].getName());
-            registerRender(log, i, "log_" + EnumHandler.Enumtype.values()[i].getName());
-            registerRender(log, i, "leave_" + EnumHandler.Enumtype.values()[i].getName());
-            registerRender(sapling, 1, "sapling_" + EnumHandler.Enumtype.values()[i].getName());
-        }
-    }
 
-     public static void registerRender(Block block, int meta, String filename) {
-        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), meta, new ModelResourceLocation(new ResourceLocation(References.MODID, filename), "inventory"));
-    }*/
 
     @SubscribeEvent
     public void registerModels(ModelRegistryEvent e) {
@@ -58,6 +49,6 @@ public class ModBlocks {
 
     @SideOnly(Side.CLIENT)
     private void registerModel(Block block) {
-        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0, new ModelResourceLocation(block.getRegistryName(), "inventory"));
+        ModelLoader.setCustomModelResourceLocation(Item.getItemFromBlock(block), 0, new ModelResourceLocation(new ResourceLocation(References.MODID, block.getUnlocalizedName().substring(5)), "inventory"));
     }
 }
