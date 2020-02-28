e648e6ae4f13e5316c319c3129514a14bbc63831
Salsabanana <59451933+Salsabanana@users.noreply.github.com>
2020-02-28 17:57:03 +0100

    Add files via upload

---
+++ src/main/java/fr/salsa/CVLST/blocks/CVLSTStairs.java
@@ -0,0 +1,19 @@
+package fr.salsa.CVLST.blocks;
+
+import fr.salsa.CVLST.ModMain;
+import fr.salsa.CVLST.init.ModItems;
+import net.minecraft.block.BlockStairs;
+import net.minecraft.block.SoundType;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.item.ItemBlock;
+
+public class CVLSTStairs extends BlockStairs {
+    public CVLSTStairs(IBlockState modelState, String name) {
+        super(modelState);
+        setRegistryName(name).setUnlocalizedName(name);
+        this.useNeighborBrightness = true;
+        this.setCreativeTab(ModMain.modtab);
+        this.setSoundType(SoundType.WOOD);
+        ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
+    }
+}
