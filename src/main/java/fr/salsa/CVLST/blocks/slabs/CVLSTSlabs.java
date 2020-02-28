5cd03efac5038daea1b90ed3a0e44fef64678850
Salsabanana <59451933+Salsabanana@users.noreply.github.com>
2020-02-28 19:07:03 +0100

    Update CVLSTSlabs.java

--- src/main/java/fr/salsa/CVLST/blocks/slabs/CVLSTSlabs.java
+++ src/main/java/fr/salsa/CVLST/blocks/slabs/CVLSTSlabs.java
@@ -1,10 +1,8 @@
-package fr.salsa.CVLST.blocks.slabs;
+package fr.salsa.CVLST.blocks;
 
 import fr.salsa.CVLST.ModMain;
 import fr.salsa.CVLST.init.ModBlocks;
-import fr.salsa.CVLST.init.ModItems;
 import fr.salsa.CVLST.utils.handler.EnumHandler;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockSlab;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
@@ -12,38 +10,36 @@ import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.properties.PropertyEnum;
 import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
-import net.minecraft.util.IStringSerializable;
+import net.minecraft.util.NonNullList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 import java.util.Random;
 
 
 public class CVLSTSlabs extends BlockSlab {
-    public static final PropertyEnum<Variant> variant = PropertyEnum.<Variant>create("variant", Variant.class);
-    Block half;
-
+    public static final PropertyEnum<EnumHandler.EnumSlab> variant = PropertyEnum.<EnumHandler.EnumSlab>create("variant", EnumHandler.EnumSlab.class);
     public boolean isDoubleSlab;
-    public CVLSTSlabs( String name, Material materialIn, BlockSlab half) {
+    public CVLSTSlabs(Material materialIn, boolean isDouble) {
         super(materialIn);
-        setRegistryName(name).setUnlocalizedName(name);
+        this.isDoubleSlab = isDouble;
         IBlockState state = this.blockState.getBaseState();
         this.useNeighborBrightness = !this.isDoubleSlab;
         this.setCreativeTab(ModMain.modtab);
         if (!this.isDouble()) state = state.withProperty(HALF, BlockSlab.EnumBlockHalf.BOTTOM);
-        this.half = half;
-        this.setDefaultState(state.withProperty(variant, Variant.DEFAULT));
+        this.setDefaultState(state.withProperty(variant, EnumHandler.EnumSlab.lupuna));
         this.setSoundType(SoundType.WOOD);
         ModBlocks.BLOCKS.add(this);
-        ModItems.ITEMS.add(new ItemBlock(this).setRegistryName(this.getRegistryName()));
     }
 
     @Override
     public String getUnlocalizedName(int meta) {
-        return super.getUnlocalizedName();
+        return super.getUnlocalizedName() + "." + EnumHandler.EnumSlab.byMetadata(meta).getName();
     }
 
     @Override
@@ -57,12 +53,12 @@ public class CVLSTSlabs extends BlockSlab {
     }
     @Override
     public Comparable<?> getTypeForItem(ItemStack stack) {
-        return Variant.DEFAULT;
+        return EnumHandler.EnumSlab.byMetadata(stack.getMetadata() & 7);
     }
     @Override
     public IBlockState getStateFromMeta(int meta)
     {
-        IBlockState state = this.blockState.getBaseState().withProperty(variant, Variant.DEFAULT);
+        IBlockState state = this.blockState.getBaseState().withProperty(variant, EnumHandler.EnumSlab.lupuna);
         if(!this.isDouble()) state = state.withProperty(HALF, ((meta&8) != 0) ? EnumBlockHalf.TOP : EnumBlockHalf.BOTTOM);
         return state;
     }
@@ -74,29 +70,35 @@ public class CVLSTSlabs extends BlockSlab {
         if(!this.isDouble() && state.getValue(HALF) == EnumBlockHalf.TOP) meta |= 8;
         return meta;
     }
+
     @Override
     public Item getItemDropped(IBlockState state, Random rand, int fortune) {
-        return Item.getItemFromBlock(half);
+        return Item.getItemFromBlock(ModBlocks.slabs);
     }
 
     @Override
     public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state) {
-        return new ItemStack(half);
+        return new ItemStack(ModBlocks.slabs);
     }
+
+    @Override
+    @SideOnly(Side.CLIENT)
+    public void getSubBlocks(CreativeTabs tab, NonNullList<ItemStack> list) {
+        if (this != ModBlocks.doubleSlabs) {
+            for (EnumHandler.EnumSlab slab : EnumHandler.EnumSlab.values()) {
+                list.add(new ItemStack(this, 1, slab.getMeta()));
+            }
+        }
+    }
+
+    @Override
+    public int damageDropped(IBlockState state) {
+        return state.getValue(variant).getMeta();
+    }
+    
 
     @Override
     protected BlockStateContainer createBlockState() {
         return this.isDouble() ? new BlockStateContainer(this, new IProperty[] {variant,HALF}): new BlockStateContainer(this, new IProperty[] {variant});
     }
-
-    public static enum Variant implements IStringSerializable {
-        DEFAULT,
-        lupuna;
-        @Override
-        public String getName() {
-            return "default";
-        }
-    }
-
-
 }
