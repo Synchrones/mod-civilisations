6007afdbecdbe56f327f7da88d68a4b66f7fcbcf
Salsabanana <59451933+Salsabanana@users.noreply.github.com>
2020-02-29 16:52:57 +0100

    Update and rename src/main/java/fr/salsa/CVLST/blocks/CVLSTSlabs.java to src/main/java/fr/salsa/CVLST/blocks/slabs/CVLSTSlabs.java

---
+++ src/main/java/fr/salsa/CVLST/blocks/slabs/CVLSTSlabs.java
@@ -0,0 +1,90 @@
+package fr.salsa.CVLST.blocks.slabs;
+
+import fr.salsa.CVLST.init.ModBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockSlab;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyEnum;
+import net.minecraft.block.state.BlockStateContainer;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.IStringSerializable;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import java.util.Random;
+
+
+public abstract class CVLSTSlabs extends BlockSlab {
+    public static final PropertyEnum<Variant> variant = PropertyEnum.<Variant>create("variant", Variant.class);
+    Block half;
+    public CVLSTSlabs(String name, Material materialIn, BlockSlab half, CreativeTabs tab) {
+        super(materialIn);
+        setUnlocalizedName(name);
+        setRegistryName(name);
+        this.useNeighborBrightness = !this.isDouble();
+        IBlockState state = this.blockState.getBaseState().withProperty(variant, Variant.DEFAULT);
+        if(!this.isDouble()) state = state.withProperty(HALF, EnumBlockHalf.BOTTOM);
+        setDefaultState(state);
+        this.half = half;
+        ModBlocks.BLOCKS.add(this);
+    }
+
+    @Override
+    public Item getItemDropped(IBlockState state, Random rand, int fortune) {
+        return Item.getItemFromBlock(half);
+    }
+
+    @Override
+    public ItemStack getItem(World worldIn, BlockPos pos, IBlockState state) {
+        return new ItemStack(half);
+    }
+
+    @Override
+    public IBlockState getStateFromMeta(int meta)
+    {
+        IBlockState state = this.blockState.getBaseState().withProperty(variant, Variant.DEFAULT);
+        if(!this.isDouble()) state = state.withProperty(HALF, ((meta&8) != 0) ? EnumBlockHalf.TOP : EnumBlockHalf.BOTTOM);
+        return state;
+    }
+
+    @Override
+    public int getMetaFromState(IBlockState state)
+    {
+        int meta = 0;
+        if(!this.isDouble() && state.getValue(HALF) == EnumBlockHalf.TOP) meta |= 8;
+        return meta;
+    }
+
+    @Override
+    protected BlockStateContainer createBlockState() {
+        return (!this.isDouble()) ? new BlockStateContainer(this, new IProperty[]{variant, HALF}): new BlockStateContainer(this, new IProperty[]{variant});
+    }
+
+
+    @Override
+    public String getUnlocalizedName(int meta) {
+        return super.getUnlocalizedName();
+    }
+
+
+    @Override
+    public IProperty<?> getVariantProperty() {
+        return variant;
+    }
+
+    @Override
+    public Comparable<?> getTypeForItem(ItemStack stack) {
+        return Variant.DEFAULT;
+    }
+
+    public static enum Variant implements IStringSerializable {
+        DEFAULT;
+        @Override
+        public String getName() {
+            return "default";
+        }
+    }
+}
