e688aebccb5047f983e22a3cd3372a34b88695cf
Salsabanana <59451933+Salsabanana@users.noreply.github.com>
2020-03-31 14:40:23 +0200

    changed arguments for custom weight

--- src/main/java/fr/salsa/CVLST/init/ModBiomes.java
+++ src/main/java/fr/salsa/CVLST/init/ModBiomes.java
@@ -1,6 +1,5 @@
 package fr.salsa.CVLST.init;
 
-import fr.salsa.CVLST.world.biomes.BiomeTest;
 import fr.salsa.CVLST.world.biomes.DenseJungle;
 import net.minecraft.world.biome.Biome;
 import net.minecraftforge.common.BiomeDictionary;
@@ -8,20 +7,17 @@ import net.minecraftforge.common.BiomeManager;
 import net.minecraftforge.fml.common.registry.ForgeRegistries;
 
 public class ModBiomes {
-    public static final Biome biometest = new BiomeTest();
     public static final Biome DenseJungle = new DenseJungle();
     public static void registerBiomes() {
-        initBiome(biometest, "biometest", BiomeManager.BiomeType.ICY, BiomeDictionary.Type.PLAINS, BiomeDictionary.Type.MOUNTAIN);
-        initBiome(DenseJungle, "Dense Jungle", BiomeManager.BiomeType.COOL, BiomeDictionary.Type.JUNGLE);
+        initBiome(DenseJungle, "Dense Jungle", BiomeManager.BiomeType.COOL, 15, BiomeDictionary.Type.JUNGLE);
 
     }
-    public static Biome initBiome(Biome biome, String name,BiomeManager.BiomeType biomeType, BiomeDictionary.Type... type) {
+    public static Biome initBiome(Biome biome, String name,BiomeManager.BiomeType biomeType,int weight, BiomeDictionary.Type... type) {
         biome.setRegistryName(name);
         ForgeRegistries.BIOMES.register(biome);
         BiomeDictionary.addTypes(biome, type);
-        BiomeManager.addBiome(biomeType, new BiomeManager.BiomeEntry(biome, 20));
+        BiomeManager.addBiome(biomeType, new BiomeManager.BiomeEntry(biome, weight));
         BiomeManager.addSpawnBiome(biome);
-        System.out.println("biomes added");
         return biome;
     }
 }
